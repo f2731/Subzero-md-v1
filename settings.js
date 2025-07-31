13a71334ec629b4c08827ff036bb08c098e9e0da
f2731 <f2731273@gmail.com>
2025-07-31 12:46:30 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+  SESSION_ID: "SUBZERO-MD;;;rFMXGI6Q#P_YatEyCrnE-iSH3dPbv8VWfpXM1p4eqmGD-regC5dE", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "923314926746", // Put Owner Number Here 
+  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+};
+
+// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
