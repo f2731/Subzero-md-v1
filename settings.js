50da7be74c36233d63465453cefab2a0826c119f
f2731 <f2731273@gmail.com>
2025-07-31 12:51:08 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
   SESSION_ID: "SUBZERO-MD;;;nUUhjaJK#ETxXEbdHim0HddV2ywbEivZa-Cl6xwzsgfJF46F2rd8", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "923071782626", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  PREFIX: "*" // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
 // CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
