218de42f4b319fcd759125deaad27f934a9433e4
f2731 <f2731273@gmail.com>
2025-07-31 12:49:01 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;rFMXGI6Q#P_YatEyCrnE-iSH3dPbv8VWfpXM1p4eqmGD-regC5dE", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "923314926746", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;nUUhjaJK#ETxXEbdHim0HddV2ywbEivZa-Cl6xwzsgfJF46F2rd8", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "923071782626", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
